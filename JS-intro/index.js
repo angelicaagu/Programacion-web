abf33b77ba56ecab2adde1d9123e1154389481f2
Angelica Agudelo <aagudelo483@soyudemedellin.edu>
2023-09-20 07:06:26 -0500

    intento de guardar

---
+++ JS-intro/index.js
@@ -0,0 +1,27 @@
+console.log("hola mundo");
+
+console.log('Esta es otra linea');
+console.log("Esta es una tercera linea");
+console.log(`Esta es otra`);
+
+console.log('y Angie dijo: "Hola"');
+console.log("y Angie dijo: 'Hola'");
+//console.log('y Angie dijo: 'Hola'');
+console.log(`Angie tiene ${2} perros`) 
+
+//Operadores logicos
+!(5 > 10 && 3 < 5)
+!(false && true)
+!(false && true)
+!(false)
+!false
+true
+
+const firstName = 'Angelica'
+const lastName = 'Agudelo'
+
+console.log(firstName + '' + lastName)
+console.log(`${firstName} ${lastName}`)
+
+const edad = prompt("Ingresa tu edad")
+alert(`Tu edad es ${edad}`)
